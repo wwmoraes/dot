700d2b4b4dda112ba0f6406689cc5f93b3fec24a
William Artero <williamwmoraes@gmail.com>
2020-11-09 12:35:27 +0100

    chore: add set and get attribute convenience methods on the interfaces

--- attributes/Reader.go
+++ attributes/Reader.go
@@ -8,6 +8,7 @@ import (
 // Reader graph object attribute read-only access
 type Reader interface {
 	GetAttribute(key Key) fmt.Stringer
+	GetAttributeString(key Key) string
 	GetAttributes() Map
 	Write(device io.Writer, mustBracket bool)
 }
--- attributes/Writer.go
+++ attributes/Writer.go
@@ -8,6 +8,12 @@ import (
 type Writer interface {
 	Reader
 	SetAttribute(key Key, value fmt.Stringer)
+	SetAttributeString(key Key, value string)
+	SetAttributeLiteral(key Key, value string)
+	SetAttributeHTML(key Key, value string)
 	SetAttributes(attributeMap Map)
+	SetAttributesString(attributeMap MapString)
+	SetAttributesLiteral(attributeMap MapString)
+	SetAttributesHTML(attributeMap MapString)
 	DeleteAttribute(key Key)
 }
