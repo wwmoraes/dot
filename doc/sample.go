804f28b806d49bda983b0084f5bfae48b3a8f12d
William Artero <williamwmoraes@gmail.com>
2020-11-11 12:33:21 +0100

    chore: add sample application and vscode launch config

---
+++ doc/sample.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/wwmoraes/dot"
+	"github.com/wwmoraes/dot/attributes"
+)
+
+func main() {
+	log.Println("creating graph instance...")
+	rootGraph := dot.NewGraph(nil)
+
+	log.Println("creating outside node...")
+	outsideGraph := rootGraph.Node("Outside")
+
+	log.Println("creating cluster subgraph A...")
+	clusterA := rootGraph.Subgraph(&dot.GraphOptions{
+		ID:      "A",
+		Cluster: true,
+	})
+	clusterA.SetAttributeString(attributes.KeyLabel, "Cluster A")
+
+	log.Println("creating node one...")
+	insideOne := clusterA.Node("one")
+
+	log.Println("creating node two...")
+	insideTwo := clusterA.Node("two")
+
+	log.Println("creating cluster subgraph B...")
+	clusterB := rootGraph.Subgraph(&dot.GraphOptions{
+		ID:      "B",
+		Cluster: true,
+	})
+	clusterB.SetAttributeString(attributes.KeyLabel, "Cluster B")
+
+	log.Println("creating node three...")
+	insideThree := clusterB.Node("three")
+
+	log.Println("creating node four...")
+	insideFour := clusterB.Node("four")
+
+	log.Println("creating edges...")
+	outsideGraph.Edge(insideFour).Edge(insideOne).Edge(insideTwo).Edge(insideThree).Edge(outsideGraph)
+
+	log.Println("opening sample.dot file for write...")
+	fd, err := os.Create("sample.dot")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println("writing graph to sample.dot...")
+	rootGraph.Write(fd)
+
+	log.Println("done!")
+}
