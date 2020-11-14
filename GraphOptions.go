d0492307fefa496491c0077b55324d18256f5dc2
William Artero <williamwmoraes@gmail.com>
2020-11-14 22:15:25 +0100

    chore: extract errors into a separate file

--- GraphOptions.go
+++ GraphOptions.go
@@ -1,25 +1,8 @@
 package dot
 
 import (
-	"errors"
-
 	"github.com/wwmoraes/dot/generators"
 )
-
-// ErrSubgraphWithoutParent means a subgraph cannot be created without a parent
-var ErrSubgraphWithoutParent = errors.New("cannot create subgraph without parent")
-
-// ErrNonSubgraphWithParent means a non-subgraph cannot be created with a parent
-var ErrNonSubgraphWithParent = errors.New("cannot create [di]graph with parent")
-
-// ErrGraphWithoutGenerator means a graph cannot be created without a generator
-var ErrGraphWithoutGenerator = errors.New("cannot create a graph without an ID generator")
-
-// ErrRootCluster means a cluster graph cannot be created at root level
-var ErrRootCluster = errors.New("cannot create a root cluster graph")
-
-// ErrInvalidParent means an invalid parent was provided
-var ErrInvalidParent = errors.New("cannot create subgraph with invalid or nil parent")
 
 // GraphOptions is implemented by values used to initialize graphs
 type GraphOptions interface {
